b1bbb8776a75fc76854cd5cd17da8b4f73dfc161
HernanWM <Waza.mango@gmail.com>
2022-04-17 23:11:14 +0000

    first commit

---
+++ src/js/component/home.js
@@ -0,0 +1,12 @@
+import React, { useState, useEffect } from "react";
+import { Item } from "./item";
+
+//create your first component
+
+export function Home() {
+	return (
+		<div>
+			<Item />
+		</div>
+	);
+}
